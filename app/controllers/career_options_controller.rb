018588773f5ad7ee53e9182a7cd3df960aa3b2d1
Tom Darcy <tdarcytom@googlemail.com>
2023-03-06 13:54:46 +0800

    controller created

---
+++ app/controllers/career_options_controller.rb
@@ -0,0 +1,2 @@
+class CareerOptionsController < ApplicationController
+end
---
+++ test/controllers/career_options_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CareerOptionsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

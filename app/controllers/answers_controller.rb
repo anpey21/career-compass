5412c19fcff48c4346407a792a5d61cec8a41603
Tom Darcy <tdarcytom@googlemail.com>
2023-03-06 15:33:49 +0800

    merged

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,9 @@
 class AnswersController < ApplicationController
+  def index
+    @answers = Answer.
+    @questions = Question.all
+    @priorities = Priority.where(user_id: current_user.id)
+    # career_options for current user
+    @career_options = CareerOption.where(user_id: current_user.id)
+  end
 end

b0407cfc24496998d1e989575cd676ad029430c5
Tom Darcy <tdarcytom@googlemail.com>
2023-03-07 13:10:10 +0800

    commit of graph progress to date

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,28 @@
+Chartkick.options = {
+  download: false,
+  labels: {
+    fontColor: "#ffffff", // change the axis label color
+  }
+  height: "400px",
+  library: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          fontColor: "#ffffff", // change the axis label color
+        },
+        gridLines: {
+          color: "#ffffff", // change the gridline color
+        },
+      }],
+      xAxes: [{
+        ticks: {
+          color: "#ffffff", // change the axis label color
+        },
+        gridLines: {
+          color: "#ffffff", // change the gridline color
+        },
+      }],
+    },
+  },
+
+}

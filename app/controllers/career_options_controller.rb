f02744d3fd589d6bdcd3c649e0a6fa9c8872e0bd
Anna Peysikova <anpey21@gmail.com>
2023-03-09 14:14:26 +0800

    what

--- app/controllers/career_options_controller.rb
+++ app/controllers/career_options_controller.rb
@@ -1,12 +1,19 @@
 class CareerOptionsController < ApplicationController
   def new
     @career_option = CareerOption.new
+    @user = current_user
   end
+
   def create
-    @career_option = CareerOption.new(career_option_params)
+    @career_option = CareerOption.new(career_options_params)
     @career_option.user = current_user
     if @career_option.save
-      redirect_to new_answer_path
+      if current_user.career_options.count == 1
+        @career_option = CareerOption.new
+        redirect_to new_career_option_path
+      elsif current_user.career_options.count == 2
+        redirect_to root_path
+      end
     else
       render :new
     end
